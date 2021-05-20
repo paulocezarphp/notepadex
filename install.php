e2fc299b1c6a59977070df03b0dc422dd94add87
Paulo <cezarleigos28@outlook.com>
2021-05-20 10:41:51 -0300

    v0.6 - Creating install
    This time we started to create the system installation panel so that soon we can start to implement the manipulation of the database to create the tables and columns that the system will use when using the system by the user.

---
+++ install.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="app/system/css/style_install.css">
+</head>
+<body>
+   
+    <?php
+      
+        $file_name = 'configx.php';
+
+        if (!file_exists($file_name)) {
+            include("app/system/page-install.php");
+        } else{
+
+        }
+   
+    ?>
+
+</body>
+</html>
