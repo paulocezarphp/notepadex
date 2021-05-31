61ff7caf73c9087f3a2220be60a113f393117123
Cezar Leigos <cezarleigos28@outlook.com>
2021-05-30 22:44:24 -0300

    v0.9.2 - Creating edit panel
    This time we started to create the editing panel so that we can edit our notes.

---
+++ app/system/javascript/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$('.item-close').click(function() {
+
+         $(this).parent().parent().hide(2000);
+
+	});
+
+});
--- app/system/page-home.php
+++ app/system/page-home.php
@@ -6,10 +6,10 @@
 		<div class="container-item" style="background: <?php echo $blocks_list[0]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-				   <div class="container-item-title-text">Notepad</div>
-			    </div>
+					<div class="container-item-title-text">Notepad</div>
+				</div>
 				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
 				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
@@ -17,14 +17,14 @@
 				iaculis qua...
 			</div>			
 		</div>
-        
-        <div class="container-item" style="background: <?php echo $blocks_list[1]; ?>;">
+
+		<div class="container-item" style="background: <?php echo $blocks_list[1]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-				   <div class="container-item-title-text">Notepad</div>
-			    </div>
+					<div class="container-item-title-text">Notepad</div>
+				</div>
 				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
 				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
@@ -36,10 +36,10 @@
 		<div class="container-item" style="background: <?php echo $blocks_list[2]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-				   <div class="container-item-title-text">Notepad</div>
-			    </div>
+					<div class="container-item-title-text">Notepad</div>
+				</div>
 				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
 				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
@@ -51,10 +51,10 @@
 		<div class="container-item" style="background: <?php echo $blocks_list[3]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-				   <div class="container-item-title-text">Notepad</div>
-			    </div>
+					<div class="container-item-title-text">Notepad</div>
+				</div>
 				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
 				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
@@ -66,10 +66,10 @@
 		<div class="container-item" style="background: <?php echo $blocks_list[4]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-				   <div class="container-item-title-text">Notepad</div>
-			    </div>
+					<div class="container-item-title-text">Notepad</div>
+				</div>
 				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
 				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
@@ -81,10 +81,10 @@
 		<div class="container-item" style="background: <?php echo $blocks_list[5]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-				   <div class="container-item-title-text">Notepad</div>
-			    </div>
+					<div class="container-item-title-text">Notepad</div>
+				</div>
 				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
 				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
@@ -96,10 +96,10 @@
 		<div class="container-item" style="background: <?php echo $blocks_list[6]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-				   <div class="container-item-title-text">Notepad</div>
-			    </div>
+					<div class="container-item-title-text">Notepad</div>
+				</div>
 				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
 				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
@@ -108,7 +108,20 @@
 			</div>			
 		</div>
 
-		
-
 	</div>
 </div>
+
+<div class="itemedite">
+	<form method="POST">
+		<div class="itemedite-group">
+			<label class="itemedite-label">Title</label>
+			<input type="" name="" class="itemedite-input">
+			<label class="itemedite-label">Context</label>
+			<textarea class="itemedite-area"></textarea>
+			<div>
+				<button type="submit" class="itemedite-submit">Save</button>
+			</div>
+		</div>
+	</form>	
+</div>	
+<div class="itemedite-background"></div>
