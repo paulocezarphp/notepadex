f8fa376836a089e0c1a6951e51ada925a9d4c65b
Cezar Leigos <cezarleigos28@outlook.com>
2021-06-03 11:10:37 -0300

    v0.9.4 - Capturing data
    This time, we start capturing the values ​​from the notebooks and throwing them into the edit panel so we can manipulate and save them.

--- app/system/javascript/script.js
+++ app/system/javascript/script.js
@@ -2,14 +2,26 @@ $(document).ready(function(){
 
 	$('.item-close').click(function() {
 
-         $(this).parent().parent().hide(2000);
+         $(this).parent().parent().parent().hide(1000);
+
+	});
+
+	$('.item-edite').click(function() {
+
+         $('#edite-painel').fadeIn(500);
+         $('.itemedite-background').fadeIn(500);
+         
+         var edite_title = "" + $(this).parent().parent().parent().children().children().children(".container-item-title-text").text();
+         var edite_context = "" + $(this).parent().parent().parent().children(".container-item-context").text();
+         $('#edite-title').val(edite_title.trim());
+         $('#edite-context').val(edite_context.trim());
 
 	});
 
 	$('#close-painel').click(function() {
 
-         $('#edite-painel').fadeOut(1000);
-         $('.itemedite-background').fadeOut(1000);
+         $('#edite-painel').fadeOut(500);
+         $('.itemedite-background').fadeOut(500);
 
 	});
 
--- app/system/page-home.php
+++ app/system/page-home.php
@@ -3,109 +3,95 @@
 
 	<div class="container-group">
 		
-		<div class="container-item" style="background: <?php echo $blocks_list[0]; ?>;">
+		<div class="container-item" style="background: <?php echo $blocks_list[0]; ?>;" data-info-color="<?php echo $blocks_list[0]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-					<div class="container-item-title-text">Notepad</div>
+					<div class="container-item-title-text">Notepad A</div>
 				</div>
-				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-edite">&#9998;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-close">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
-				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
-				fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, 
-				iaculis qua...
+				A trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, iaculis qua...
 			</div>			
 		</div>
 
-		<div class="container-item" style="background: <?php echo $blocks_list[1]; ?>;">
+		<div class="container-item" style="background: <?php echo $blocks_list[1]; ?>;" data-info-color="<?php echo $blocks_list[1]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-					<div class="container-item-title-text">Notepad</div>
+					<div class="container-item-title-text">Notepad B</div>
 				</div>
-				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-edite">&#9998;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-close">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
-				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
-				fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, 
-				iaculis qua...
+				A trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, iaculis qua...
 			</div>			
 		</div>
 
-		<div class="container-item" style="background: <?php echo $blocks_list[2]; ?>;">
+		<div class="container-item" style="background: <?php echo $blocks_list[2]; ?>;" data-info-color="<?php echo $blocks_list[2]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-					<div class="container-item-title-text">Notepad</div>
+					<div class="container-item-title-text">Notepad C</div>
 				</div>
-				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-edite">&#9998;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-close">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
-				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
-				fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, 
-				iaculis qua...
-			</div>			
+				A trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, iaculis qua...
+			</div>				
 		</div>
 
-		<div class="container-item" style="background: <?php echo $blocks_list[3]; ?>;">
+		<div class="container-item" style="background: <?php echo $blocks_list[3]; ?>;" data-info-color="<?php echo $blocks_list[3]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-					<div class="container-item-title-text">Notepad</div>
+					<div class="container-item-title-text">Notepad D</div>
 				</div>
-				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-edite">&#9998;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-close">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
-				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
-				fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, 
-				iaculis qua...
-			</div>			
+				A trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, iaculis qua...
+			</div>				
 		</div>
 
-		<div class="container-item" style="background: <?php echo $blocks_list[4]; ?>;">
+		<div class="container-item" style="background: <?php echo $blocks_list[4]; ?>;" data-info-color="<?php echo $blocks_list[4]; ?>;">
 			<div class="container-item-group">
-				<div class="container-item-title">
-					<div class="container-item-title-text">Notepad</div>
+				<div class="container-item-title ">
+					<div class="container-item-title-text">Notepad E</div>
 				</div>
-				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-edite">&#9998;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-close">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
-				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
-				fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, 
-				iaculis qua...
-			</div>			
+				A trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, iaculis qua...
+			</div>				
 		</div>
 
-		<div class="container-item" style="background: <?php echo $blocks_list[5]; ?>;">
+		<div class="container-item" style="background: <?php echo $blocks_list[5]; ?>;" data-info-color="<?php echo $blocks_list[5]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-					<div class="container-item-title-text">Notepad</div>
+					<div class="container-item-title-text">Notepad F</div>
 				</div>
-				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-edite">&#9998;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-close">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
-				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
-				fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, 
-				iaculis qua...
-			</div>			
+				A trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, iaculis qua...
+			</div>				
 		</div>
 
-		<div class="container-item" style="background: <?php echo $blocks_list[6]; ?>;">
+		<div class="container-item" style="background: <?php echo $blocks_list[6]; ?>;" data-info-color="<?php echo $blocks_list[6]; ?>;">
 			<div class="container-item-group">
 				<div class="container-item-title">
-					<div class="container-item-title-text">Notepad</div>
+					<div class="container-item-title-text">Notepad G</div>
 				</div>
-				<div class="container-item-icon"><span class="item-icon">&#9998;</span></div>
-				<div class="container-item-icon item-close"><span class="item-icon">&#10006;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-edite">&#9998;</span></div>
+				<div class="container-item-icon"><span class="item-icon item-close">&#10006;</span></div>
 			</div>
 			<div class="container-item-context">
-				trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis 
-				fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, 
-				iaculis qua...
-			</div>			
+				A trum quam lorem quisque etiam ultrices tellus ut, leo tincidunt dictum turpis fames curabitur aliquam. hendrerit platea risus eleifend quam leo litora eleifend, iaculis qua...
+			</div>				
 		</div>
 
 	</div>
@@ -118,9 +104,9 @@
 		<div class="itemedite-group">
 
 			<label class="itemedite-label">Title</label>
-			<input type="" name="" class="itemedite-input">
+			<input type="" name="" class="itemedite-input" id="edite-title">
 			<label class="itemedite-label">Context</label>
-			<textarea class="itemedite-area"></textarea>
+			<textarea class="itemedite-area" id="edite-context"></textarea>
 			<div class="itemedite-button-group">
 				<div class="itemedite-close" id="close-painel">Close</div>
 				<div></div>
@@ -129,4 +115,6 @@
 		</div>
 	</form>	
 </div>	
+
+
 <div class="itemedite-background"></div>
